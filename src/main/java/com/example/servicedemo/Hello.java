9f6d58e1a436cdb8dcc392523a261a884f7bb3a2
wing <x.wing.luo@gmail.com>
2023-02-26 21:50:30 +0800

    flux

--- src/main/java/com/example/servicedemo/Hello.java
+++ src/main/java/com/example/servicedemo/Hello.java
@@ -1,15 +1,16 @@
 package com.example.servicedemo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class Hello {
 
-    @RequestMapping(value="hello",method = RequestMethod.GET)
-    public String hello() {
+    //    @RequestMapping(value="hello",method = RequestMethod.GET)
+    @GetMapping("/hello")
+    public Mono<String> hello() {   // 【改】返回类型为Mono<String>
 
-        return "hello this is spring boot test";
+        return Mono.just("Welcome to reactive world ~");     // 【改】使用Mono.just生成响应式数据
     }
 }
--- src/main/java/com/example/servicedemo/config/RouterFunctionConfiguration.java
+++ src/main/java/com/example/servicedemo/config/RouterFunctionConfiguration.java
@@ -1,5 +1,39 @@
 package com.example.servicedemo.config;
 
+//import com.example.servicedemo.domain.User;
+//import com.example.servicedemo.repository.UserRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.reactive.function.server.RequestPredicates;
+//import org.springframework.web.reactive.function.server.RouterFunction;
+//import org.springframework.web.reactive.function.server.RouterFunctions;
+//import org.springframework.web.reactive.function.server.ServerResponse;
+//import reactor.core.publisher.Flux;
+//
+//import java.util.Collection;
+//@Configuration
+//public class RouterFunctionConfiguration {
+//
+//    /**
+//     * person/find/all
+//     * @param userRepository
+//     * @return
+//     */
+//    @Bean
+//    @Autowired
+//    public RouterFunction<ServerResponse> personFindAll(UserRepository userRepository){
+//      return   RouterFunctions.route(RequestPredicates.GET("/person/find/all"), request -> {
+//            Collection<User> users=userRepository.findAll();
+//            Flux<User> userFlux=Flux.fromIterable(users);
+//           return ServerResponse.ok().body(userFlux,User.class);
+//
+//        });
+//
+//    }
+//}
+
+
 import com.example.servicedemo.domain.User;
 import com.example.servicedemo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,23 +46,43 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
 
 import java.util.Collection;
+
+/**
+ * <br>
+ * 标题: 路由器函数配置<br>
+ * 描述: 配置映射<br>
+ *
+ * @author zc
+ * @date 2018/04/08
+ */
 @Configuration
 public class RouterFunctionConfiguration {
 
     /**
-     * person/find/all
-     * @param userRepository
-     * @return
+     * Servlet 规范
+     * 请求接口：ServletRequest 或者 HttpServletRequest
+     * 响应接口：ServletResponse 或者 HttpServletResponse
+     * <p>
+     * Spring 5.0 重新定义了服务请求的响应接口
+     * 请求接口：ServerRequest
+     * 响应接口：ServerResponse
+     * 即可支持 Servlet 规范，也可以支持自定义，比如 Netty （Web Server）
+     * 以本例
+     * 定义 GET 请求，并且放回所有的用户对象 URI：/person/find/all
+     * Flux 是 0 - N 个对象集合
+     * Mono 是 0 - 1 个对象集合
+     * Reactive 中的 Flux 或者 Mono 是异步处理（非阻塞）
+     * 集合对象基本上是同步处理（阻塞）
+     * Flux 或者 Mono 都是 Publisher
      */
     @Bean
     @Autowired
-    public RouterFunction<ServerResponse> personFindAll(UserRepository userRepository){
-      return   RouterFunctions.route(RequestPredicates.GET("/person/find/all"), request -> {
-            Collection<User> users=userRepository.findAll();
-            Flux<User> userFlux=Flux.fromIterable(users);
-           return ServerResponse.ok().body(userFlux,User.class);
-
-        });
-
+    public RouterFunction<ServerResponse> personFindAll(UserRepository userRepository) {
+        Collection<User> users = userRepository.findAll();
+        return RouterFunctions.route(RequestPredicates.GET("/person/find/all"),
+                request -> {
+                    Flux<User> userFlux = Flux.fromIterable(users);
+                    return ServerResponse.ok().body(userFlux, User.class);
+                });
     }
 }
