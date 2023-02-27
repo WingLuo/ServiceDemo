8d14c3bb3abecc29459c1f1e524b67eee1e51c70
Luo-yong-ext <luo.yong.ext@razer.com>
2023-02-27 10:46:05 +0800

    三个接口调通

--- src/main/java/com/example/servicedemo/Hello.java
+++ src/main/java/com/example/servicedemo/Hello.java
@@ -1,6 +1,7 @@
 package com.example.servicedemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -10,7 +11,6 @@ public class Hello {
     //    @RequestMapping(value="hello",method = RequestMethod.GET)
     @GetMapping("/hello")
     public Mono<String> hello() {   // 【改】返回类型为Mono<String>
-
         return Mono.just("Welcome to reactive world ~");     // 【改】使用Mono.just生成响应式数据
     }
 }
--- src/main/java/com/example/servicedemo/controller/UserController.java
+++ src/main/java/com/example/servicedemo/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.servicedemo.domain.User;
 import com.example.servicedemo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class UserController {
@@ -14,16 +15,16 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
-
     @PostMapping("/person/save")
 //    @RequestMapping(value="/person/save",method = RequestMethod.POST)
-    public User save(@RequestParam String name) {
+    //这种写法是url后面带参数  法克
+    public User save(@RequestParam(value = "name",required = false) String name) {
         User user = new User();
         user.setName(name);
-        if (userRepository.save(user)){
-            System.out.printf("用户对象 %s 存储成功\n",user);
+        if (userRepository.save(user)) {
+            System.out.printf("用户对象 %s 存储成功\n", user);
         }
         return user;
-
     }
+
 }
